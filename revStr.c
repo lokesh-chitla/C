fdad5d28a891b6f9286a09e013e0879ab64dd9cf
Lokesh Chitla <53783706+lokesh-iot@users.noreply.github.com>
2019-10-18 15:56:39 +0530

    Update revStr.c
    removed debug stats

--- revStr.c
+++ revStr.c
@@ -7,23 +7,13 @@ int main()
     int len = 0, i =0;
     while(str[len] != '\0') len++;
     len--;  // end : len-1;
-    //printf("%d : %s\n", len, str);
-    char str2[len];
     while((len - i) >= 2)
     {
         tmp     =   str[len];
         str[len]=   str[i];
         str[i]  =   tmp;
         len--; i++;
-        
-        /* To store in another str :
-        str2[i]= str[len];
-        len--; i++;
-        */
     }
     printf("%s\n", str);
-    //str2[i]='\0';
-    //printf("%s\n", str2);
-
     return 0;
 }
